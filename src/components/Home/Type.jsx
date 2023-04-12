713f85d56006ed93a38f6431e5b0495178013ac8
prabakaran <mspk028@gmail.com>
2023-04-12 11:27:05 +0530

    ...

--- src/components/Home/Type.jsx
+++ src/components/Home/Type.jsx
@@ -5,7 +5,7 @@ const Type = () => {
   return (
     <Typewriter
       options={{
-        strings: ["< React Js Developer />", "< Effective Problem Solver />"],
+        strings: ["< React Js Developer />", "<Effective Problem Solver/>"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
