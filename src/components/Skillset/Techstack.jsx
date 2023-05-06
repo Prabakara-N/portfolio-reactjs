90379841a8e045bb0f94525a7730162a5f91847b
Prabakaran-M <mspk028@gmail.com>
2023-05-06 18:37:51 +0530

    ...

--- src/components/Skillset/Techstack.jsx
+++ src/components/Skillset/Techstack.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJavascript1, DiReact, DiNodejs } from "react-icons/di";
+import { DiJavascript1, DiReact } from "react-icons/di";
 import {
   SiTailwindcss,
   SiRedux,
